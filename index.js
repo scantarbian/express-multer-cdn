c0adc06a3ee93ae8814859b9716e7726493d97fa
scantarbian <davidpamereanb@gmail.com>
2021-06-12 00:44:25 +0700

    change mkdirSync to mkdir, can delete files

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const multer = require('multer')
 const cors = require('cors')
+const fs = require('fs');
 
 //express variables
 const app = express()
 const port = process.env.PORT
 const corsOptions = {
-    origin: process.env.TARGET_URL,
-    optionsSuccessStatus: 200
+  origin: process.env.TARGET_URL,
+  optionsSuccessStatus: 200
 }
 
 app.use(cors(corsOptions))
 
 app.listen(port, () => {
-    console.log(`CDN active at http://localhost:${port}`)
+  console.log(`CDN active at http://localhost:${port}`)
 })
 
 app.use(express.static('public'), )
@@ -24,34 +25,51 @@ app.options('*', cors(corsOptions))
 
 //multer settings
 const imageStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, `public/uploads/${req.body.owner}`,)
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    },
+  destination: function (req, file, cb) {
+    const path = `public/uploads/${req.body.owner}`
+    fs.mkdir(path,{recursive: true})
+    cb(null, path)
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  },
 })
 
 const docsStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, `public/uploads/${req.body.owner}/${req.body.request}`,)
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    },
+  destination: function (req, file, cb) {
+    const path = `public/uploads/${req.body.owner}/${req.body.request}`
+    fs.mkdir(path,{recursive: true})
+    cb(null, path)
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  },
 })
 
 const uploadImage = multer({ storage: imageStorage })
 const uploadDocs = multer({ storage: docsStorage })
 
 app.post('/image', uploadImage.single("profileImage"), (req,res,next)=>{
-    res.status(200).send({
-        url: req.file.path
-    })
+  res.status(200).send({
+    url: req.file.path
+  })
 })
 
 app.post('/docs', uploadDocs.single("attachment"), (req,res,next)=>{
-    res.status(200).send({
-        url: req.file.path
-    })
+  res.status(200).send({
+    url: req.file.path
+  })
+})
+
+app.delete('/docs', (req, res) => {
+  const body = JSON.parse(req.body)
+  const path = `public/uploads/${body.owner}/${body.request}/${body.fileName}`
+
+  fs.unlink(path, (err) => {
+    if (err) {
+      console.error(err)
+      res.status(500).send(err)
+    }
+    res.status(200).send('FILE DELETED')
+  })
 })
